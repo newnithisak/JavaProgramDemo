d135da66c07b21f96dae5cf802218ef417684e79
newnithisak <newnithisak@gmail.com>
2018-09-11 15:53:45 +0700

    Add Setting and Mean

--- Main/Mean.java
+++ Main/Mean.java
@@ -10,9 +10,11 @@ public class Mean {
         try {
             int n = in.nextInt();
             double sum = 0 ;
-            for (int i = 0; i < n; i++) {
-            System.out.print("Enter Number : ");
-            sum += in.nextInt();}
+            for (int i = 0; i < n; i++) 
+            {
+                System.out.print("Enter Number : ");
+                sum += in.nextInt();
+            }
 
             System.out.println("******Mean : " + sum/n);
         }
